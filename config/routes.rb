0fedddabbe3cefc0b3a9846d078290858b54ed7c
山岸宗奨 <yamagishi@yamagishisoususumunoMacBook-puro.local>
2018-09-10 13:23:08 +0900

    ログインサインアップ設定

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # devise_for:usersがdeviseを使用する際にURLとしてuserを含むことを示します
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'root#top' 
+end
