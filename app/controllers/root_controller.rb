3cb609c5e6e18eb4f8e27bf6045111a5caa475a2
山岸宗奨 <yamagishi@yamagishisoususumunoMacBook-puro.local>
2018-09-10 15:35:58 +0900

    ログインサインアップ設定finish

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,6 @@
 class RootController < ApplicationController
+	before_action :authenticate_user!
 	def top
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # devise_for:usersがdeviseを使用する際にURLとしてuserを含むことを示します
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # ログイン認証に成功した場合にリダイレクトされるパス
   get 'top' => 'root#top' 
 end
